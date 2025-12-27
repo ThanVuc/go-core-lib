b804f25410d2e73430b92d95f360255c1cea3ff2
sinh <sinh@gmail.com>
2025-12-27 11:27:32 +0700

    [CRONJOB] SCD-246: Add wait gracefull shutdow

--- cronjob/cronmanager.go
+++ cronjob/cronmanager.go
@@ -18,7 +18,11 @@ func (m *CronManager) AddScheduler(s CronScheduler) {
 	m.schedulers = append(m.schedulers, s)
 }
 
-func (m *CronManager) Shutdown() {
+func (m *CronManager) Shutdown(wg *sync.WaitGroup) {
+	if wg != nil {
+		defer wg.Done()
+	}
+
 	m.mu.Lock()
 	defer m.mu.Unlock()
 	for _, s := range m.schedulers {
